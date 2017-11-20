16e411fe5477362db2b05b141db61b68292172f8
Mantas <kazlauskas.mantas.dev@gmail.com>
2017-11-20 16:33:06 +0200

    Added extension to add all nescesary configs
    Extension that takes a given service name and adds all files needed for the service to work properly (global settings, secrets, service configs and appsetings.json)

--- BalticAmadeus.Config/ConfigurationBuilderExtensions.cs
+++ BalticAmadeus.Config/ConfigurationBuilderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace BalticAmadeus.Config
 {
@@ -13,6 +14,21 @@ namespace BalticAmadeus.Config
         public static IConfigurationBuilder AddCloudConfig(this IConfigurationBuilder builder, string serviceName, bool required)
         {
             builder.Add(new BaConfigSource(serviceName, required));
+            return builder;
+        }
+        
+        public static IConfigurationBuilder AddServiceConfigurations(this IConfigurationBuilder builder, string serviceName)
+        {
+            var serviceBlobName = serviceName + ".json";
+            var serviceSecretBlobName = serviceName + "Secrets.json";
+
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddCloudConfig("GlobalSettings.json")
+                .AddCloudConfig("GlobalSettingsSecrets.json")
+                .AddCloudConfig(serviceBlobName)
+                .AddCloudConfig(serviceSecretBlobName);
+
             return builder;
         }
     }
